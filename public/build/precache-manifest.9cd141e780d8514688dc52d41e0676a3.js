45e03455f06c381d806d41e1cef92fc1261e943b
Radek <radekk0002@gmail.com>
2021-07-10 22:28:28 +0200

    Add projects files

---
+++ public/build/precache-manifest.9cd141e780d8514688dc52d41e0676a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ac7946cbe97d296cb25798a759673d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1334ed9c6c7b7e8404d3",
+    "url": "/static/css/main.b7c41eb0.chunk.css"
+  },
+  {
+    "revision": "69fe2bc8801ff3fff245",
+    "url": "/static/js/2.04dac7a8.chunk.js"
+  },
+  {
+    "revision": "1334ed9c6c7b7e8404d3",
+    "url": "/static/js/main.c94041ce.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "78ccde2fc832c2abd92a80db06cfc548",
+    "url": "/static/media/background.78ccde2f.jpg"
+  }
+]);
